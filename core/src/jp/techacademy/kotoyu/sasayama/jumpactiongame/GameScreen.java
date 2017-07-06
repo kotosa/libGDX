02761a4a90d0dc44aec5b9d4c78c763cff6fe0bd
kotosa <royalmilktea_36@yahoo.co.jp>
2017-07-07 00:07:46 +0900

    Lesson7 5.1のサンプルファイル。
    画像が出ない。
    落ちるがエラーは出ていない。
    worningは発生。
    W/MALI: __egl_set_error:110: [WARNING]Mali EGL errorcode: 3004

--- core/src/jp/techacademy/kotoyu/sasayama/jumpactiongame/GameScreen.java
+++ core/src/jp/techacademy/kotoyu/sasayama/jumpactiongame/GameScreen.java
@@ -8,20 +8,33 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class GameScreen extends ScreenAdapter{
+
+    static final float CAMERA_WIDTH = 10;
+    static final float CAMERA_HEIGHT = 15;
+
     private JumpActionGame mGame;
 
     Sprite mBg;
+    OrthographicCamera mCamera;
+    FitViewport mViewPort;
 
     public GameScreen(JumpActionGame game){
         mGame = game;
-        Texture bgTexture = new Texture("backk.png");
+        Texture bgTexture = new Texture("back.png");
         mBg = new Sprite(new TextureRegion(bgTexture, 0, 0, 540, 810));
         mBg.setPosition(0, 0);
+
+        mCamera = new OrthographicCamera();
+        mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+        mViewPort = new FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT, mCamera);
+
     }
 
     @Override
@@ -29,9 +42,20 @@ public class GameScreen extends ScreenAdapter{
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // カメラの座標をアップデートしてスプライトの表示に反映させる
+        mCamera.update();
+        mGame.batch.setProjectionMatrix(mCamera.combined);
+
         mGame.batch.begin();
+        mBg.setPosition(mCamera.position.x - CAMERA_WIDTH/2, mCamera.position.y - CAMERA_HEIGHT/2);
         mBg.draw(mGame.batch);
         mGame.batch.end();
     }
+
+    @Override
+    public void resize(int width, int height){
+        mViewPort.update(width, height);
+    }
+
 
 }
